443824530daeaf51679370c42653ebde48d85cf8
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2013-07-14 19:25:39 +0530

    Completed Carousel and TagCloud examples.

--- beginners-guide/chapter08/src/main/java/com/packtpub/techbuzz/controllers/TagController.java
+++ beginners-guide/chapter08/src/main/java/com/packtpub/techbuzz/controllers/TagController.java
@@ -2,6 +2,7 @@ package com.packtpub.techbuzz.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -9,10 +10,15 @@ import javax.faces.context.FacesContext;
 
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
+import org.primefaces.model.tagcloud.DefaultTagCloudItem;
+import org.primefaces.model.tagcloud.DefaultTagCloudModel;
+import org.primefaces.model.tagcloud.TagCloudItem;
+import org.primefaces.model.tagcloud.TagCloudModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.packtpub.techbuzz.entities.Post;
 import com.packtpub.techbuzz.entities.Tag;
 import com.packtpub.techbuzz.services.BuzzService;
 
@@ -31,11 +37,23 @@ public class TagController
 	private List<Tag> selectedTags = new ArrayList<Tag>();
 	private Tag selectedTag = new Tag();
 	
+	private static Random random = new Random();
+	private TagCloudModel model; 
+	private List<Post> posts;
+	
 		
 	@PostConstruct
 	void init()
 	{
 		tags = buzzService.findAllTags();
+		
+		model = new DefaultTagCloudModel();  
+        posts = buzzService.findAllPosts();
+        for (Tag tag : tags)
+		{
+        	model.addTag(new DefaultTagCloudItem(tag.getLabel(), getStrength(1, 5)));
+		}
+        
 	}
 	
 	public void setSelectedTag(Tag selectedTag)
@@ -86,6 +104,27 @@ public class TagController
 	{
 		Tag unselectedObj = (Tag) unselectEvent.getObject();
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("You have unselected :"+unselectedObj.getLabel()));
+	}
+	
+	public TagCloudModel getModel() {  
+        return model;  
+    }  
+      
+    public void onSelect(SelectEvent event) {  
+        TagCloudItem item = (TagCloudItem) event.getObject();  
+        String label = item.getLabel();
+        FacesMessage msg = new FacesMessage("Selected Tag: "+ item.getLabel());  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        posts = buzzService.findPostsByTag(label);
+    }
+    
+    public List<Post> getPosts()
+	{
+		return posts;
+	}
+    private int getStrength(int low, int high)
+	{
+    	return random.nextInt(high-low) + low;
 	}
 	
 }
