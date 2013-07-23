ddfae2b8fea4e18e13ccd637c0085d8088da929d
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2013-07-23 17:21:30 +0530

    Added PanelGrid, ScrollPanel and AccordionPanel examples.

--- beginners-guide/chapter09/src/main/java/com/packtpub/techbuzz/controllers/SampleController.java
+++ beginners-guide/chapter09/src/main/java/com/packtpub/techbuzz/controllers/SampleController.java
@@ -1,11 +1,15 @@
 package com.packtpub.techbuzz.controllers;
 
+import java.util.Date;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
 import org.primefaces.event.CloseEvent;
+import org.primefaces.event.TabChangeEvent;
+import org.primefaces.event.TabCloseEvent;
 import org.primefaces.event.ToggleEvent;
 
 /**
@@ -31,4 +35,33 @@ public class SampleController
     private void addMessage(FacesMessage message) {  
         FacesContext.getCurrentInstance().addMessage(null, message);  
     }
+    
+    public String getTab1Content(){
+    	//System.out.println("*****Tab1 Content*******");
+    	return "Tab1 Content" + new Date();
+    }
+    
+    public String getTab2Content(){
+    	//System.out.println("*****Tab2 Content*******");
+    	return "Tab2 Content"+ new Date();
+    }
+    
+    public String getTab3Content(){
+    	//System.out.println("*****Tab3 Content*******");
+    	return "Tab3 Content"+ new Date();
+    }
+    
+    public void onTabChange(TabChangeEvent event) {  
+        FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab:" + event.getTab().getId());  
+  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+  
+    public void onTabClose(TabCloseEvent event) {  
+        FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());  
+  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }
+    
+    
 }
